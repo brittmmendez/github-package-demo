ffb2f68a782f3f916f6226182569ef077675c917
Brittany Morris <brittmmendez@gmail.com>
2021-12-07 15:14:00 -0500

    m

---
+++ mathematics/multiplying.py
@@ -0,0 +1,12 @@
+"""
+A collection of adding
+"""
+
+def multiply_by_1(num):
+    return 1 * num
+
+def multiply_by_2(num):
+    return 2 * num
+
+def multiply_by_num(num1, num2):
+    return num1 * num2
