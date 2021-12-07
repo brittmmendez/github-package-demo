0a3a91fec177e5cf577a4d973e9989b7856b8a98
Brittany Morris <brittmmendez@gmail.com>
2021-12-07 15:26:17 -0500

    m

---
+++ mathematics/hubbleFunctionsNew.py
@@ -0,0 +1,12 @@
+def get_file_name_without_path_and_extension(value):
+    a = value.split("/")
+    fileName = a[len(a)-1]
+    try:
+        pos = fileName.rindex(".")
+        return fileName[0:pos]
+    except Exception as e:
+        print(e)
+        return fileName
+
+def testCaching():
+    print("will this deploy?")
